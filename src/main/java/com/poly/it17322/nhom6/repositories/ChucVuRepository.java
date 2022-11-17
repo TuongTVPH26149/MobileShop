283a4ea58ab4aff088d7f35680232f4918d581d4
TuongTVPH26149 <tuongtvph26149@fpt.edu.vn>
2022-11-17 15:16:35 +0700

    add class repository, crud taikhoan,chucvu,trangthaitaikhoan

--- src/main/java/com/poly/it17322/nhom6/repositories/ChucVuRepository.java
+++ src/main/java/com/poly/it17322/nhom6/repositories/ChucVuRepository.java
@@ -4,10 +4,66 @@
  */
 package com.poly.it17322.nhom6.repositories;
 
+import com.poly.it17322.nhom6.domainmodels.ChucVu;
+import com.poly.it17322.nhom6.domainmodels.ChucVu;
+import com.poly.it17322.nhom6.utilities.HibernatUtil;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author LiamTrieu
  */
 public class ChucVuRepository {
-    
+
+    public List<ChucVu> SelectALLChucVu() {
+        List<ChucVu> lstChucVu = new ArrayList<>();
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Query query = session.createQuery("FROM ChucVu", ChucVu.class);
+            lstChucVu = query.getResultList();
+        }
+        return lstChucVu;
+    }
+
+    public ChucVu SelectChucVuById(UUID id) {
+        ChucVu chucVu = new ChucVu();
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Query query = session.createQuery("FROM ChucVu WHERE id = :id", ChucVu.class);
+            query.setParameter("id", id);
+            chucVu = (ChucVu) query.getSingleResult();
+        } catch (Exception e) {
+        }
+        return chucVu;
+    }
+
+    public boolean InsertChucVu(ChucVu chucVu) {
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Transaction tran = session.getTransaction();
+            tran.begin();
+            session.save(chucVu);
+            tran.commit();
+            return true;
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    public boolean UpdateChucVu(ChucVu chucVu) {
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Transaction tran = session.getTransaction();
+            tran.begin();
+            ChucVu tk = chucVu;
+            tk.setLastModifiedDate(new Date());
+            session.saveOrUpdate(chucVu);
+            tran.commit();
+            return true;
+        } catch (Exception e) {
+        }
+        return false;
+    }
 }
--- src/main/java/com/poly/it17322/nhom6/repositories/TaiKhoanRepository.java
+++ src/main/java/com/poly/it17322/nhom6/repositories/TaiKhoanRepository.java
@@ -4,10 +4,66 @@
  */
 package com.poly.it17322.nhom6.repositories;
 
+import com.poly.it17322.nhom6.domainmodels.TaiKhoan;
+import com.poly.it17322.nhom6.utilities.HibernatUtil;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author LiamTrieu
  */
 public class TaiKhoanRepository {
-    
+
+    public List<TaiKhoan> SelectALLTaiKhoan() {
+        List<TaiKhoan> lstTaiKhoan = new ArrayList<>();
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Query query = session.createQuery("FROM TaiKhoan", TaiKhoan.class);
+            lstTaiKhoan = query.getResultList();
+        } catch (Exception e) {
+        }
+        return lstTaiKhoan;
+    }
+
+    public TaiKhoan SelectTaiKhoanById(UUID id) {
+        TaiKhoan taiKhoan = new TaiKhoan();
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Query query = session.createQuery("FROM TaiKhoan WHERE id = :id", TaiKhoan.class);
+            query.setParameter("id", id);
+            taiKhoan = (TaiKhoan) query.getSingleResult();
+        } catch (Exception e) {
+        }
+        return taiKhoan;
+    }
+
+    public boolean InsertTaiKhoan(TaiKhoan taiKhoan) {
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Transaction tran = session.getTransaction();
+            tran.begin();
+            session.save(taiKhoan);
+            tran.commit();
+            return true;
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    public boolean UpdateTaiKhoan(TaiKhoan taiKhoan) {
+        try ( Session session = HibernatUtil.getFACTORY().openSession()) {
+            Transaction tran = session.getTransaction();
+            tran.begin();
+            TaiKhoan tk = taiKhoan;
+            tk.setLastModifiedDate(new Date());
+            session.saveOrUpdate(taiKhoan);
+            tran.commit();
+            return true;
+        } catch (Exception e) {
+        }
+        return false;
+    }
 }
