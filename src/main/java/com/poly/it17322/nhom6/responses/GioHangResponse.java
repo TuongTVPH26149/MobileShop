91ad97a4a10a41e7468da78ad253cb530f0f0919
TuongTVPH26149 <tuongtvph26149@fpt.edu.vn>
2022-11-20 18:10:51 +0700

    lay lai code

---
+++ src/main/java/com/poly/it17322/nhom6/responses/GioHangResponse.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.it17322.nhom6.responses;
+
+import com.poly.it17322.nhom6.domainmodels.HoaDonChiTiet;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author LiamTrieu
+ */
+@Getter
+@Setter
+public class GioHangResponse {
+
+    private String tenSP;
+    private int soLuong;
+    private BigDecimal giaBan;
+
+    public GioHangResponse(HoaDonChiTiet hdct) {
+        this.tenSP = hdct.getTenSP();
+        this.soLuong = hdct.getSoLuong();
+        this.giaBan = hdct.getGia();
+    }
+    
+    public Object[] toDataRow(){
+    return new Object[]{this.tenSP, this.soLuong,this.giaBan};
+}
+}
---
+++ src/main/java/com/poly/it17322/nhom6/services/IBanHangService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.poly.it17322.nhom6.services;
+
+import com.poly.it17322.nhom6.responses.GioHangResponse;
+import com.poly.it17322.nhom6.responses.HoaDonBanHangResponse;
+import com.poly.it17322.nhom6.responses.SanPhamBanHangResponse;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author LiamTrieu
+ */
+public interface IBanHangService {
+
+    List<HoaDonBanHangResponse> getHoaDonTreo();
+
+    List<SanPhamBanHangResponse> getAllSpBh();
+
+    boolean createHoaDon(UUID idNV);
+    
+    List<GioHangResponse> getGioHang(UUID idHD, UUID idCTSP);
+}
